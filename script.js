32025b1c23dba0a93f27e5345e5d488318935a24
PeleBahalag <102541131+PeleBahalag@users.noreply.github.com>
2022-07-12 12:43:28 +0300

    script changes

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let videoElement;
 let vidSource;
 let lastLogo;
+let globeBtn;
+let img;
 
 let srcs = ["tzriffin-pin", "bahalag-pin", "277-pin", "727-pin", "47-pin", "33-pin", "414-pin", "636-pin", "41-pin", "869-pin", "595-pin"];
 
@@ -8,10 +10,14 @@ window.addEventListener("load", () => {
     videoElement = document.getElementById("vid");
     vidSource = document.getElementById("source-vid");
     lastLogo = document.getElementById("tzriffin");
+    globeBtn = document.getElementById("globe");
+    img = document.getElementById("imgisrael");
     startUp();
 });
 
 const startUp = () => {
+    videoElement.style.display = 'block';
+    img.style.display = 'none';
     // get all the gdoods
     let gdoodim = document.getElementsByClassName("logos");
     for (let i = 0; i < gdoodim.length; i++) {
@@ -19,9 +25,24 @@ const startUp = () => {
             changeGdood(i);
         });
     }
+
+    // Set up the globe button
+    globeBtn.addEventListener("click", map);
+
+}
+
+const map = () => {
+    lastLogo.classList.remove("selected-logo");
+    vidSource.setAttribute("src", ``);
+    videoElement.load();
+    videoElement.style.display = 'none';
+    img.style.display = 'block';
+
 }
 
 const changeGdood = (i) => {
+    videoElement.style.display = 'block';
+    img.style.display = 'none';
     // Change the visibility class on the logos above
     lastLogo.classList.remove("selected-logo");
     event.currentTarget.classList.add("selected-logo");
